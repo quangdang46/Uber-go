6e561d2a54c66d4a87119037dc58ce2fc06a324c
Tran Quang Dang <tranquangdang21@gmail.com>
2025-08-01 21:38:52 +0700

    Implement graceful shutdown for API Gateway server with signal handling and context timeout

--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"ride-sharing/shared/env"
 )
@@ -22,7 +27,31 @@ func main() {
 		Handler: mux,
 		Addr:    httpAddr,
 	}
-	if err := server.ListenAndServe(); err != nil {
-		log.Printf("HTTP server error:%v", err)
+
+	serverError := make(chan error, 1)
+	go func() {
+		log.Printf("Server listening on %s", httpAddr)
+		serverError <- server.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+
+	case err := <-serverError:
+		log.Printf("Error starting the server :%v", err)
+	case sig := <-shutdown:
+		log.Printf("Server is shutting down due to %v signal", sig)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("Could not stop the server %v", err)
+			server.Close()
+		}
+
 	}
+
 }
