a239c228c49149da72b068ef13aa855112782bc2
Tran Quang Dang <tranquangdang21@gmail.com>
2025-08-04 22:30:44 +0700

    Add driver registration and unregistration methods in Service; implement thread-safe access with mutex. Generate geohash for driver location using predefined routes.

--- services/driver-service/internal/service/service.go
+++ services/driver-service/internal/service/service.go
@@ -1,17 +1,57 @@
 package service
 
-import pb "ride-sharing/shared/proto/driver"
+import (
+	math "math/rand/v2"
+	"ride-sharing/services/driver-service/pkg/utils"
+	pb "ride-sharing/shared/proto/driver"
+	"sync"
 
-type Service struct {
-	drivers []*driverInMap
-}
+	"github.com/mmcloughlin/geohash"
+)
 
 type driverInMap struct {
 	Driver *pb.Driver
+}
+
+type Service struct {
+	drivers []*driverInMap
+	mu      sync.RWMutex
 }
 
 func NewService() *Service {
 	return &Service{
 		drivers: make([]*driverInMap, 0),
 	}
+}
+
+func (s *Service) RegisterDriver(driverId string, packageSlug string) (*pb.Driver, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	randomIndex := math.IntN(len(utils.PredefinedRoutes))
+	randomRoute := utils.PredefinedRoutes[randomIndex]
+
+	// we can ignore this property for now, but it must be sent to the frontend.
+	geohash := geohash.Encode(randomRoute[0][0], randomRoute[0][1])
+
+	driver := &pb.Driver{
+		Geohash:  geohash,
+		Location: &pb.Location{Latitude: randomRoute[0][0], Longitude: randomRoute[0][1]},
+		Name:     "Lando Norris",
+		// Id: ...,
+		// PackageSlug:    packageSlug,
+		// ProfilePicture: randomAvatar,
+		// CarPlate:       randomPlate,
+	}
+
+	// TODO: Add driver to list
+
+	return driver, nil
+}
+
+func (s *Service) UnregisterDriver(driverId string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	// TODO: Filter driver from list
 }
